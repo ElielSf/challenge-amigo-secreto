aa7da18547944c385bc42c95bac06c4d21c8be0b
Eliel Souza <elielsf13@gmail.com>
2025-07-28 14:36:45 -0300

    feat: create 'draw friend'

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaAmigos = [];
+let tamanhoLista;
+
+let indiceSorteado;
+let nomeSorteado;
+
 let nome;
 
 function limparCampo() {
@@ -9,7 +14,9 @@ function limparCampo() {
 function visualizarLista(nome) {
     let lista = document.getElementById("listaAmigos");
     let novoItem = document.createElement("li");
+
     novoItem.innerHTML = nome;
+
     lista.appendChild(novoItem);
 }
 
@@ -25,4 +32,27 @@ function adicionarAmigo() {
     visualizarLista(nome);
 
     limparCampo();
+}
+
+function sortearAmigo() {
+    tamanhoLista = listaAmigos.length;
+
+    if (tamanhoLista === 0) {
+        return alert("A lista de amigos está vazia.");
+    }
+
+    indiceSorteado = parseInt((Math.random() * tamanhoLista));
+
+    nomeSorteado = listaAmigos[indiceSorteado];
+
+    listaAmigos = [];
+
+    document.getElementById("listaAmigos").innerHTML = "";
+
+    let lista = document.getElementById("resultado");
+    let novoItem = document.createElement("li");
+
+    novoItem.innerHTML = `O amigo secreto sorteado é: ${nomeSorteado}`;
+
+    lista.appendChild(novoItem);
 }
