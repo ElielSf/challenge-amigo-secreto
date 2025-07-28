4ff5bd0d50cdae0be5620ad60cb835c751963598
Eliel Souza <elielsf13@gmail.com>
2025-07-28 13:59:23 -0300

    feat: create 'add friends'

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+let nome;
+
+function adicionarAmigo() {
+    nome = document.querySelector("input").value;
+
+    if (nome === "") {
+        return alert("Digite um nome válido.");
+    }
+
+    listaAmigos.push(nome);
+}
